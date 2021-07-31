80a6c9a7a78f752f5fb6455b81654e5be4224e5e
100BC <73491141+100BC@users.noreply.github.com>
2021-07-31 06:22:27 -0600

    Add Header Scrolling

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
 const Header = () => {
+  const [headerDark, setHeaderDark] = useState(false);
+
+  const listenScrollEvent = () => {
+    if (window.scrollY > 73) {
+      return setHeaderDark(true);
+    }
+    return setHeaderDark(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+
+    return () => window.removeEventListener('scroll', listenScrollEvent);
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={[styles.header, `${headerDark ? styles.headerDark : ''}`].join(' ')}>
       <nav className={styles.desktopNav}>
         <Link href="/">
           <a title="Overview">Overview</a>
